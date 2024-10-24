98c88810dd69e0924ffb841d22ef0e9b072b83ad
root <root@Main-Index-Operator>
2024-10-24 17:33:48 +1100

    Scripted C Type Commits

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ * Printf - Is saying words
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,")
+		return (0);
+}
