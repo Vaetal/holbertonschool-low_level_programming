7c66e7fb3d58ec141952d45c72e4b5ea42f557ee
root <root@Main-Index-Operator>
2024-10-24 17:19:39 +1100

    Scripted C Type Commits

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*
-* File does what it looks like it does
-*/
+/**
+ * main - Entry point
+ * puts - Another print command
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
