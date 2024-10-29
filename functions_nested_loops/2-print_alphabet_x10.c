d191bc31f5b8de89214d907ba29c1d53607ab9d0
root <root@Main-Index-Operator>
2024-10-29 15:07:36 +1100

    Scripted C Type Commits

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Check holberton
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+ *
+ * Description: This function uses _putchar to print the alphabet 
+ * in lowercase, followed by a new line, 10 times.
+ * 
  * Return: Nothing.
  */
-
 void print_alphabet_x10(void)
 {
-		char i;
+	    char i;
 	        int j;
 
-		for (j = 0; j < 10; j++)
-						{
-	        for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-		_putchar('\n');
-												}
+		    for (j = 0; j < 10; j++)
+			        {
+					        for (i = 'a'; i <= 'z'; i++)
+							        {
+									            _putchar(i);
+										            }
+						        _putchar('\n');
+							    }
 }
+
