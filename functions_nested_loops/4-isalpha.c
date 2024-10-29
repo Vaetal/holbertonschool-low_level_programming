e55bb67a9ac9bdd2ae496c0666604123b0ee1700
root <root@Main-Index-Operator>
2024-10-29 16:55:53 +1100

    Scripted C Type Commits

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -11,15 +11,15 @@
 int _isalpha(int c)
 {
 		char lower, upper;
-			int isletter = 0;
+		int isletter = 0;
 
-				for (lower = 'a'; lower <= 'z'; lower++)
+		for (lower = 'a'; lower <= 'z'; lower++)
 						{
-									for (upper = 'A'; upper <= 'Z'; upper++)
-												{
-																if (c == lower || c == upper)
-																					isletter = 1;
-																		}
-										}
-					return (isletter);
+							for (upper = 'A'; upper <= 'Z'; upper++)
+		                {
+					if (c == lower || c == upper)
+						isletter = 1;
+}
+}
+return (isletter);
 }
