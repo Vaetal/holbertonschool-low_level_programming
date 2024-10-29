5365f484c7d2a8ac8cecb796cac67c744515c82d
root <root@Main-Index-Operator>
2024-10-29 20:48:57 +1100

    Scripted C Type Commits

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,26 +12,3 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void putformat(int);
-
-	  /*  r = print_sign(98);
-	      _putchar(',');
-	        _putchar(' ');
-		_putchar(r + '0');
-		_putchar('\n');
-		r = print_sign(0);
-		_putchar(',');
-	        _putchar(' ');
-                _putchar(r + '0');
-	        _putchar('\n');
-	        r = print_sign(0xff);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(r + '0');
-		_putchar('\n');
-		r = print_sign(-1);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(r + '0');
-		_putchar('\n');
-		return (0);
-	       	*/
