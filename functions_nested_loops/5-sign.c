a3a548f2aa368ef1929251ade9a09c2d7cb72324
root <root@Main-Index-Operator>
2024-10-29 16:26:07 +1100

    Scripted C Type Commits

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Check Holberton
  * @n: An input number
- * Description: Tohis function prints the sign of the number
+ * Description This function prints the sign of the number
  * Return: 1 if number is positive, o if number is 0, or
  * -1 if number is negative
  */
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -38,6 +38,6 @@ int main(void)
 		_putchar(',');
 		_putchar(' ');
 		_putchar(r + '0');
-		_putchar('\n)
+		_putchar('\n');
 		return (0);
 }
