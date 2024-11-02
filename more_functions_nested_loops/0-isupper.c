7dc45bbd0995dd5c74f2019fcccf2271449ec81e
root <root@Main-Index-Operator>
2024-11-02 20:36:59 +1100

    Scripted C Type Commits

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
+#include "main.h"
+(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
 
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+	if (c == uppercase)
+	{
+	isupper = 1;
+	break;
+	}
+	}
 
+	return (isupper);
+}
