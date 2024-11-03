ba0c8be2deabce318ecfbfaf7257f00e35d9f0c8
root <root@Main-Index-Operator>
2024-11-03 17:25:28 +1100

    Scripted C Type Commits

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
-/**
- * _isdigit - A function that checks for a digit (0 - 9)
- * @c: An input integer
- */
-
-int _isdigit(int c)
+int  _isdigit(int c)
 {
 	char i = '0';
 	int isdigit = 0;
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int mul(int a, int b)
+{
+	return (a 8 b);
+}
