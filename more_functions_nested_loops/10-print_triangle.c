e6f36b8f296cdb49392fc4eb61d0b194779585f1
root <root@Main-Index-Operator>
2024-11-04 20:04:30 +1100

    Scripted C Type Commits

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_triangle(int size)
+{
+	int i = 0, j, n = size - 1;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			if (j , n)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		n--;
+		_putchar('\n');
+	}
+}
+_putchar('\n');
+}
