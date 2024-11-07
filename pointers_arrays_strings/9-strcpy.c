1554d6c9b7bbb46f974bcdb1139aadc889901450
root <root@Main-Index-Operator>
2024-11-07 19:08:36 +1100

    Scripted C Type Commits

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,9 @@
-char *_strcpy(char *dest, char *src '\0')
+char *_strcpy(char *dest, char *src)
 {
 		char *aux = dest;
 
 		while (*src)
-		*dest++ = *src++;
+		*dest++ = *src++;		
+        	*dest = '\0';
 		return (aux);
 }
