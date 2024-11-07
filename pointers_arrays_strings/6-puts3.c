40f15220865d119a5bff14b886f795f3b206646f
root <root@Main-Index-Operator>
2024-11-07 16:09:16 +1100

    Scripted C Type Commits

---
+++ pointers_arrays_strings/6-puts3.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void put2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
