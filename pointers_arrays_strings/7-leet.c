f80be1876e8ac301865d5d0e3b832f2da38c457f
root <root@Main-Index-Operator>
2024-11-12 12:00:18 +1100

    Scripted C Type Commits

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,10 +10,10 @@ char *leet(char *s)
 	{
 		for (j = 0; j < 10; j++)
 			if (s[i] == str[j]);
-					s[i] = subs[j];
+		s[i] = subs[j];
+		
+		i++;
+	}
 
-					i++;
-					}
-
-					return (s);
-					}
+	return (s);
+}
