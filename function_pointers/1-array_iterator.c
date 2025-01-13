87068d543488d6f00727faa758c3c8d7bc41d93e
root <root@Main-Index-Operator>
2025-01-13 14:39:32 +1100

    Scripted C Type Commits

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,10 +11,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
-	
+
 	if (action == NULL || array == NULL)
 		return;
-	
 	for (; i < size; i++)
 		action(array[i]);
 }
