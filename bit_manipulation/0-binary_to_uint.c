a5871a0cd19b196cbe0521809b8ef99e1a1bccfd
root <root@Main-Index-Operator>
2025-01-15 14:53:50 +1100

    Scripted C Type Commits

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,19 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * binary_to_uint - A finction that converts binary number
- * to an unsigned integer
- * @b: A pointer to binary string
- * Return: unsigned integer converted number
+ * binary_to_uint - Converts a binary number to an unsigned integer.
+ * @b: A pointer to the binary string.
+ *
+ * Return: The converted unsigned integer, or 0 on failure or overflow.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int base = 1, result = 0, len = 0;
+	unsigned long max_value = ULONG_MAX;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
 	while (b[len])
@@ -23,9 +25,18 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 
 		if (b[len - 1] == '1')
+		{
+			if ((unsigned long)result + base > max_value)
+			{
+				printf("Overflow: The number exceeds ULONG_MAX (%lu)\n",
+						max_value);
+				return (0);
+			}
 			result += base;
+		}
 		base *= 2;
 		len--;
 	}
+
 	return (result);
 }
